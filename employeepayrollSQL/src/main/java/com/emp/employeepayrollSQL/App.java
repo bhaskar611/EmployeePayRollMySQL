a38de6c9c843d7dfbd6da8b541e3a865b236d73a
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-03-10 19:07:28 +0530

    Added:

---
+++ employeepayrollSQL/src/main/java/com/emp/employeepayrollSQL/App.java
@@ -0,0 +1,13 @@
+package com.emp.employeepayrollSQL;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
